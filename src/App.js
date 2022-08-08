ac53e3a4fd14cb7519851b780044e91a3f4f0138
Ben Fristad <bpfristad@gmail.com>
2022-08-08 16:18:12 -0600

    cleaned up App.js and added specified locations to make props

--- src/App.js
+++ src/App.js
@@ -12,45 +12,83 @@ export default function App() {
   return (
     <Box>
       <Router>
-        <Navbar title={title} buttons={buttons} links={links} />
+        <Navbar title={title} buttons={buttons} links={dashboardLinks} />
         <Routes>
-          <Route path="/" element={<Dashboard cardTitle={cardTitle} cardImage={cardImage} links={links}/>} />
-          <Route path="/gamelibrary" element={<GameLib />} />
-          <Route path="/sodofaculty" element={<SodoFaculty />} />
-          <Route path="*" element={<ErrorPage />} />
+          <Route 
+            path="/" 
+            element={<Dashboard 
+                      cardTitle={dashboardCardTitle} 
+                      cardImage={dashboardCardImage} 
+                      links={dashboardLinks}/>} 
+          />
+          <Route 
+            path="/game-library" 
+            element={<GameLib />} 
+          />
+          <Route 
+            path="/sodo-faculty" 
+            element={<SodoFaculty />} 
+          />
+          <Route 
+            path="/hardware-inventory" 
+            element={<HardwareInventory 
+                      cardTitle={hardwareInventoryCardTitles} 
+                      cardImage={hardwareInventoryCardImages}
+                      cardLinks={hardwareInventoryLinks}
+                    />} 
+          />
+          <Route 
+            path="*" 
+            element={<ErrorPage />} 
+          />
         </Routes>
       </Router>
     </Box>
   )
 }
+// DASHBOARD
+/*================================================================================================================================================*/
+
 const title = 'Dashboard';
 
-
 const buttons = [
-  'ert',
-  'game library',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
-  'settings',
+  'Home',
+  'Game Library',
+  'Sodo Faculty',
+  'Hardware Inventory'
 ];
 
-const links = ["/", "/gamelibrary"]
+const dashboardLinks = ["/", "/game-library", "/sodo-faculty", "/hardware-inventory", "*"]
 
-const cardTitle = ['Home', 'Game Library', 'SODO Faculty', 'hello Worldsickomode', 'yeeto'];
+const dashboardCardTitle = ['Home', 'Game Library', 'Sodo Faculty', 'Hardware Inventory'];
 
-const cardImage = ["https://th.bing.com/th/id/OIP.PUqMC9V3pmHgOdoOFnjDXAHaIP?w=170&h=189&c=7&r=0&o=5&dpr=1.25&pid=1.7", 
-"https://th.bing.com/th/id/OIP.Z68HVcuLOtTZWAtbvDBbGAHaEz?w=256&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7",
-"https://th.bing.com/th/id/OIP.dMc05QaZqP6XPF3hLUwcNAHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
-"https://th.bing.com/th/id/OIP.Q7mkWzwCZuwRnflarqfH5wHaEM?w=326&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7",
-"https://th.bing.com/th/id/OIP.btX8Jn-zsDwq0el8oatqZgHaLH?w=126&h=190&c=7&r=0&o=5&dpr=1.25&pid=1.7"];
+const dashboardCardImage = [
+  "https://th.bing.com/th/id/OIP.PUqMC9V3pmHgOdoOFnjDXAHaIP?w=170&h=189&c=7&r=0&o=5&dpr=1.25&pid=1.7", 
+  "https://th.bing.com/th/id/OIP.Z68HVcuLOtTZWAtbvDBbGAHaEz?w=256&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  "https://th.bing.com/th/id/OIP.dMc05QaZqP6XPF3hLUwcNAHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  "https://th.bing.com/th/id/OIP.Q7mkWzwCZuwRnflarqfH5wHaEM?w=326&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  "https://th.bing.com/th/id/OIP.btX8Jn-zsDwq0el8oatqZgHaLH?w=126&h=190&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+];
+
+// GAME LIBRARY
+/*================================================================================================================================================*/
 
 
+// SODO FACULTY
+/*================================================================================================================================================*/
+
+
+// HARDWARE INVENTORY
+/*================================================================================================================================================*/
+
+const hardwareInventoryLinks = ["/thanos", "/recrobot", "/cypher", "/pepper"];
+
+const hardwareInventoryCardTitles = ['Thanos', 'Rec Robot', 'Cypher', 'Pepper'];
+
+const hardwareInventoryCardImages = [
+  "https://th.bing.com/th/id/OIP.PUqMC9V3pmHgOdoOFnjDXAHaIP?w=170&h=189&c=7&r=0&o=5&dpr=1.25&pid=1.7", 
+  "https://th.bing.com/th/id/OIP.Z68HVcuLOtTZWAtbvDBbGAHaEz?w=256&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  "https://th.bing.com/th/id/OIP.dMc05QaZqP6XPF3hLUwcNAHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  "https://th.bing.com/th/id/OIP.Q7mkWzwCZuwRnflarqfH5wHaEM?w=326&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  "https://th.bing.com/th/id/OIP.btX8Jn-zsDwq0el8oatqZgHaLH?w=126&h=190&c=7&r=0&o=5&dpr=1.25&pid=1.7"
+];
