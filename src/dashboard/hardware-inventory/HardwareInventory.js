2c73d2ffa61115a90431f9737a417bdc500d73ee
Ben Fristad <bpfristad@gmail.com>
2022-08-08 13:30:45 -0600

    added GUI for hardware inventory page

---
+++ src/dashboard/hardware-inventory/HardwareInventory.js
@@ -0,0 +1,75 @@
+import { Box, Grid, InputBase, styled, Typography } from '@mui/material'
+import React from 'react'
+import DashCardCircular from '../../components/DashCardCircular'
+import Navbar from '../../components/Navbar'
+
+  const title = 'Hardware Inventory'
+
+  const buttons = [
+      'Bruh',
+      'yes',
+  ]
+
+  const StyledBox = styled(Box)({
+    display: 'flex',
+    justifyContent: 'center',
+  })
+
+  const RightJustifyBox = styled(Box)({
+    display: 'flex',
+    justifyContent: 'end',
+  })
+
+  const Search = styled("div")(({ theme }) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    border: '1px solid grey',
+    borderRadius: theme.shape.borderRadius, // sets the border radius to the same as the theme
+    width: '250px',
+  }));
+  
+  export default function HardwareInventory({cardTitle, cardImage}) {
+    return (
+      <Box>
+        <Navbar title={title} buttons={buttons} />
+        <RightJustifyBox
+          paddingTop={3}
+          paddingRight={3}
+        >
+              <Search><InputBase placeholder="search..."/></Search>
+        </RightJustifyBox>
+        <StyledBox 
+          flexGrow={1}
+          paddingTop={5}
+        >
+          <Grid
+            container
+            spacing={0}
+            paddingLeft={1}
+            direction="column"
+            alignItems="center"
+            style={{ minHeight: '100vh' }}
+            width={1008}
+          >
+            <Typography 
+              variant='h4'
+              paddingBottom={2}
+              textAlign='center'
+            >
+              Welcome to the Hardware Inventory</Typography>
+            <Typography>Choose a Category</Typography>
+            <Grid
+              container
+              spacing={1}
+              padding={1}
+              direction="row"
+              gap={8}
+              paddingTop={5}
+            > 
+              <DashCardCircular cardImage={cardImage} cardTitle={cardTitle}/>
+            </Grid>
+          </Grid>
+        </StyledBox> 
+      </Box>
+    )
+  }
