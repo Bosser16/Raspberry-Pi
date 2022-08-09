27746fdd85c2baad206a2daddffed7fa7bef3b8a
Ben Fristad <bpfristad@gmail.com>
2022-08-09 09:30:56 -0600

    started adding svgs to assets folder

---
+++ src/assets/Module-Loader.js
@@ -0,0 +1,3 @@
+export { default as GameLib } from './dashboard/GameLib'
+export { default as Dashboard } from './dashboard/Dashboard'
+export { default as HardwareInventory } from './hardware-inventory/HardwareInventory'
