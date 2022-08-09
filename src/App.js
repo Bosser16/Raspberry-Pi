a843a01b57da585974548cf82bb654efedb2fe40
Ben Fristad <bpfristad@gmail.com>
2022-08-09 09:34:58 -0600

    changed images to svgs

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import GameLib from './components/GameLib';
 import ErrorPage from './components/ErrorPage';
 import SodoFaculty from './dashboard/sodo-faculty/SodoFaculty';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import * as Images from './assets/Module-Loader'
 
 export default function App() {
   return (
@@ -19,7 +20,8 @@ export default function App() {
             element={<Dashboard 
                       cardTitle={dashboardCardTitle} 
                       cardImage={dashboardCardImage} 
-                      links={dashboardLinks}/>} 
+                      links={dashboardLinks}
+                    />} 
           />
           <Route 
             path="/game-library" 
@@ -63,11 +65,10 @@ const dashboardLinks = ["/", "/game-library", "/sodo-faculty", "/hardware-invent
 const dashboardCardTitle = ['Home', 'Game Library', 'Sodo Faculty', 'Hardware Inventory'];
 
 const dashboardCardImage = [
-  "https://th.bing.com/th/id/OIP.PUqMC9V3pmHgOdoOFnjDXAHaIP?w=170&h=189&c=7&r=0&o=5&dpr=1.25&pid=1.7", 
-  "https://th.bing.com/th/id/OIP.Z68HVcuLOtTZWAtbvDBbGAHaEz?w=256&h=186&c=7&r=0&o=5&dpr=1.25&pid=1.7",
-  "https://th.bing.com/th/id/OIP.dMc05QaZqP6XPF3hLUwcNAHaEK?w=302&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7",
+  <Images.Dashboard size={180} />,
+  <Images.GameLib size={180} />,
+  <Images.HardwareInventory size={180} />,
   "https://th.bing.com/th/id/OIP.Q7mkWzwCZuwRnflarqfH5wHaEM?w=326&h=185&c=7&r=0&o=5&dpr=1.25&pid=1.7",
-  "https://th.bing.com/th/id/OIP.btX8Jn-zsDwq0el8oatqZgHaLH?w=126&h=190&c=7&r=0&o=5&dpr=1.25&pid=1.7"
 ];
 
 // GAME LIBRARY
